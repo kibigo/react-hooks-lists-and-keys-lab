a4863ad10a51e7ba6862af348a1a92c44e455650
kibigo <jacksonkibigo99@gmail.com>
2023-08-25 13:38:58 +0300

    Completed Assignment

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -3,10 +3,20 @@ import ProjectItem from "./ProjectItem";
 
 function ProjectList({ projects }) {
   console.log(projects);
+
+  const projectName = projects.map((project) => {
+    return (
+      <div key={project.id}>
+        <h2>{project.name}</h2>
+        <p>{project.about}</p>
+        <p>{project.technologies.join(', ')}</p>
+      </div>
+    )
+  })
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list">{projectName}</div>
     </div>
   );
 }
